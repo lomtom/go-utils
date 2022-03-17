3b6beb9f2ef0a838ee08a98972d04780f56a2475
lomtom <ouyang@xlauncher.io>
2022-03-17 15:45:18 +0800

    refactor(root): 修改 job开启后立马执行一次任务（死锁）

--- job/job.go
+++ job/job.go
@@ -104,6 +104,8 @@ func (j *timerJob) Start() error {
 	ticker := time.NewTicker(j.interval)
 	log.Printf("%v 第%v次  开始执行任务", j.name, j.count+1)
 	go func() {
+		// 开启后，立马触发一次任务
+		j.jf(j)
 		for {
 			select {
 			case <-ticker.C:
@@ -114,8 +116,6 @@ func (j *timerJob) Start() error {
 			}
 		}
 	}()
-	// 开启后，立马触发一次任务
-	j.jf(j)
 	return nil
 }
 
